ca5abd5ed25bcd7c5386da5fd0a6d91a4ad3782e
lihlcnkr <lihlcnkr@gmail.com>
2017-04-23 22:12:31 +0900

    add Vector

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Data.Set
 
 import BathroomStalls
 import System.Environment
--- src/BathroomStalls.hs
+++ src/BathroomStalls.hs
@@ -1,37 +1,46 @@
 module BathroomStalls where
 
-import Data.List
+
 import Data.Char
---import Data.SortedList(SortedList)
---import qualified Data.SortedList as S
+import Data.Sequence(Seq)
+import qualified Data.Sequence as S
 
 getBathroomStalls :: String -> String
 getBathroomStalls xs =
   let inputs = words xs
       ys = [read (head inputs)::Int]
       y = read (last inputs)::Int
-      (maxLR,minLR) = stallLRSet ys y
+      (maxLR,minLR) = stallLRSet (S.fromList ys) y
   in show maxLR ++ " " ++ (show minLR)
 
-stallLRSet:: [Int] -> Int -> (Int, Int)
+stallLRSet:: Seq Int -> Int -> (Int, Int)
 stallLRSet xs y
-  |y > length xs= stallLRSet (concat $ fmap splitStall xs) (y - (length xs))
-  |otherwise = (z, head zs)
-     where z:zs = splitStall ((sortBy (flip compare) xs) !! (y-1))
-  
---  | y > length xs +1 = stallLRSet (sortBy (flip compare) $ concat $ fmap splitStall (x:xs)) (y - 1 - (length xs))
---  | otherwise = stallLRSet (splitStallAndPut x xs) (y -1)
+  |y <= fsSize= (retx, rety)
+  |otherwise = stallLRSet (insertNtime fsSize rety (insertNtime fsSize retx ts)) (y - fsSize)
+     where
+       x = S.index xs 0
+       (fs,ts) = S.breakl (/= x) xs
+       (retx, rety) = splitStall x
+       fsSize = length fs
 
 
-splitStall::Int -> [Int]
-splitStall x = (x -1 - halfX):halfX:[]
+insertNtime :: Ord a=> Int -> a -> Seq a -> Seq a
+insertNtime n x (S.viewl -> S.EmptyL) = S.fromList $ replicate n x
+insertNtime n x ys@(S.viewl -> (y S.:< ys'))
+  = case compare x y of
+      LT -> y S.<| insertNtime n x ys'
+      _ -> foldr (S.<|) ys (replicate n x)
+
+insertBy :: Ord a => (a -> a -> Ordering) -> a -> Seq a -> Seq a
+insertBy _   x (S.viewl -> S.EmptyL) = S.singleton x
+insertBy cmp x ys@(S.viewl -> (y S.:< ys'))
+ = case cmp x y of
+     GT -> y S.<| insertBy cmp x ys'
+     _  -> x S.<| ys
+
+
+
+splitStall::Int -> (Int, Int)
+splitStall x = ((x -1 - halfX), halfX)
   where
     halfX = div (x-1) 2
-
-splitStallAndPut::Int -> [Int] -> [Int]
-splitStallAndPut x xs = insertBy (flip compare) leftX xsx
-  where
-    halfX = div (x-1) 2
-    xsx = insertBy (flip compare) halfX xs
-    leftX = x -1 - halfX
-
